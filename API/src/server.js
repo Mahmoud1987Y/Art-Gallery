e470e99d2f582a03ecfcceb7e27a9511462463a0
mahmoud abou elnaga <mahmoudyoussif111@gmail.com>
2024-10-04 10:13:59 +0300

    add collect system matrics data using prom-client

--- API/src/server.js
+++ API/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const client = require("prom-client");
 const helmet = require("helmet");
 const cors = require("cors");
 const { logging } = require("./helper/logging");
@@ -9,7 +10,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.get("/matrics", async (req, res) => {
+  const register = new client.Registry();
+  register.setDefaultLabels({ app: "art_gallery" });
+  client.collectDefaultMetrics({ register });
+  res.setHeader("Content-Type", register.contentType);
 
+  register.metrics().then((data) => res.status(200).send(data));
+});
 app.listen(PORT, () => {
   logging.info(`connection done successifuly on local host on port ${PORT}`);
   console.log(`connection done successifuly on local host on port ${PORT}`);
