d4048e1a6da2c817171d56d9ba988845ff03e3a7
mahmoud abou elnaga <mahmoudyoussif111@gmail.com>
2024-10-05 13:14:07 +0300

    add validation module

--- API/src/controllers/usersController.js
+++ API/src/controllers/usersController.js
@@ -1,17 +1,24 @@
 const logging = require("../helper/logging");
 const { Users } = require("../models/Users");
+const { validate } = require("../helper/validation");
 
 exports.getUsers = async (req, res, next) => {
   try {
-    
     const result = Users.findAll();
     res.status(400).json({ status: "OK", data: result, error: {} });
   } catch (error) {
     next(error); // Forward the error to the error handler
   }
 };
-exports.getUserByEmail = (req, res, next) => {
-  res.send("getUserByEmail");
+exports.getUserByEmail = async (req, res, next) => {
+  const data = req.body;
+  const validateData = await validate(data);
+  if (validateData.error) {
+    res.send(validateData.error.details[0].message);
+  } else {
+    
+    res.send("getUserByEmail");
+  }
 };
 exports.addUser = (req, res, next) => {
   res.send("addUser");
---
+++ API/src/helper/validation.js
@@ -0,0 +1,30 @@
+const Joi = require("joi");
+
+exports.validate = (data = []) => {
+  const schemeInputs = {
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .min(6)
+      .max(30)
+      .required()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .message("enter a valid password must contain only letters and numbers"),
+    first_name: Joi.string().min(2).max(30).required().lowercase(),
+    last_name: Joi.string().min(2).max(30).required().lowercase(),
+    pic_url: Joi.string(),
+    date_of_birth: Joi.date().required(),
+    role: Joi.string().allow("admin", "customer", "moderator").required(),
+    preferred_language: Joi.string().allow("ar", "en").default("en"),
+  };
+  const targetInput = {};
+  for (item in data) {
+    if (schemeInputs.hasOwnProperty(item)) {
+      targetInput[item] = schemeInputs[item];
+    }
+  }
+
+  const scheme = Joi.object(targetInput);
+  console.log(scheme.validate(data));
+  return scheme.validate(data);
+};
