1d35ffbcb2e00e6810f9a33c45a9b87e55b95372
mahmoud abou elnaga <mahmoudyoussif111@gmail.com>
2024-10-05 10:42:10 +0300

    update error handler file

--- API/src/middlewares/errorHandler.js
+++ API/src/middlewares/errorHandler.js
@@ -1,8 +1,18 @@
-const { logging } = require("../helper/logging");
-exports.errorHandler = (error, req, res, next) => {
-  if (error) {
-    logging.error(error);
-    res.status(500).send("server error");
-  }
-  next();
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.status || 500; // Default to 500 if no status
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack, // Only show stack trace in development
+  });
+};
+const notFoundHandler = (req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+};
+
+module.exports = {
+  errorHandler,
+  notFoundHandler,
 };
