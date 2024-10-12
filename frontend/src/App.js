669d8018cedc4fda8510939420b964057e01db45
mahmoud abou elnaga <mahmoudyoussif111@gmail.com>
2024-10-12 13:20:09 +0300

    add routes to application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import logo from "./logo.svg";
 
 import "./App.css";
-import Header from "./common/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collections from "./pages/Collections";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className=" px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
   );
 }
