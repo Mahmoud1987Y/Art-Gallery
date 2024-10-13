b20ee66dfcea1aa9eabed48e1d17baa14f3f1281
mahmoud <mahmoudyoussif111@gmail.com>
2024-10-13 12:01:23 +0300

    add hero section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,6 +4,7 @@ import cart_icon from "./shopping-bag.png";
 import profile_icon from "./profile.png";
 import menu_icon from "./menu.png";
 import back_icon from "./back.png";
+import hero_img from "./hero.jpg";
 
 export const assets = {
   logo,
@@ -12,4 +13,5 @@ export const assets = {
   profile_icon,
   menu_icon,
   back_icon,
+  hero_img,
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className=" flex justify-between items-center py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="logo" />
-      <ul className=" hidden sm:flex gap-5 text-sm text-gray-700">
+      <ul className=" hidden md:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>Home</p>
           <hr className=" w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
