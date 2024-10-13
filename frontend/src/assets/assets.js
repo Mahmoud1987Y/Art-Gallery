4b80370741acc7156c8f8b5498a4711e8054a04c
mahmoud abou elnaga <mahmoudyoussif111@gmail.com>
2024-10-13 06:15:03 +0300

    add responsive menu

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,10 +2,14 @@ import logo from "./logo.png";
 import search_icon from "./search-icon.png";
 import cart_icon from "./shopping-bag.png";
 import profile_icon from "./profile.png";
+import menu_icon from "./menu.png";
+import back_icon from "./back.png";
 
 export const assets = {
   logo,
   search_icon,
   cart_icon,
   profile_icon,
+  menu_icon,
+  back_icon,
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className=" flex justify-between items-center py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="logo" />
@@ -32,12 +33,73 @@ const Navbar = () => {
             alt="profile image"
           />
           <div className=" group-hover:block hidden absolute dropdown-menu right-1/2 pt-0">
-            <div className="flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-500  rounded">
-              <p className="cursor-pointer hover:text-black">My profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+            <div className="flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-500 rounded-md">
+              <p className="cursor-pointer  text-center hover:text-black">
+                My profile
+              </p>
+              <p className="cursor-pointer text-center hover:text-black">
+                Orders
+              </p>
+              <p className="cursor-pointer text-center hover:text-black">
+                Logout
+              </p>
             </div>
           </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-7 min-w-5" alt="" />
+          <p className=" absolute right-[-5px] bottom-[-5px] text-center leading-4 bg-red-600 text-white aspect-square rounded-full text-[10px]   ">
+            10
+          </p>
+        </Link>
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-7 cursor-pointer md:hidden"
+          alt="menu"
+        />
+      </div>
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img src={assets.back_icon} className="w-7" alt="" />
+            <p>back</p>
+          </div>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/"
+            className="flex flex-col items-center gap-4 py-5 hover:bg-gray-400"
+          >
+            <p>Home</p>
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/collections"
+            className="flex flex-col items-center gap-4 py-5 hover:bg-gray-400"
+          >
+            <p>Collections</p>
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/about"
+            className="flex flex-col items-center gap-4 py-5 hover:bg-gray-400"
+          >
+            <p>About Us</p>
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/contact"
+            className="flex flex-col items-center gap-4 py-5 hover:bg-gray-400"
+          >
+            <p>Contact Us</p>
+          </NavLink>
         </div>
       </div>
     </div>
